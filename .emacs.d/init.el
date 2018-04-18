641cb3936af3f94f9d06d7ea78b6a586787a1bc9
Soonho Kong <soonho.kong@tri.global>
2018-04-18 16:42:35 -0400

    Add .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,41 @@
+;;; Emacs Initialization file
+;;; Soonho Kong <soonho.kong@gmail.com>
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Ignore "following symbolic link warning"
+(setq vc-handled-backends nil)
+
+;; use-package
+(unless (package-installed-p 'use-package) (package-refresh-contents)
+  (package-install 'use-package)
+  (package-install 'diminish)
+  (package-install 'bind-key))
+(setq use-package-always-ensure t)
+(setq use-package-verbose t)
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
+
+(use-package org
+  :ensure org-plus-contrib
+  :config
+    (setq org-src-tab-acts-natively t)
+    (setq org-src-fontify-natively t))
+
+;; ----------------------
+;; Load settings.org file
+;; ----------------------
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "settings.org")))
+
+;; -------------------------
+;; Custom
+;; -------------------------
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
+(load custom-file)
+
