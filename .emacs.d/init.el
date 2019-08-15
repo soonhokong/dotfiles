d43c27bf6aeae630b40ca0814e8b4712d9038485
Soonho Kong <soonho.kong@tri.global>
2019-08-15 09:08:34 -0400

    emacs: fix gnu repo problem

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 ;;; Emacs Initialization file
 ;;; Soonho Kong <soonho.kong@gmail.com>
 (require 'package)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
