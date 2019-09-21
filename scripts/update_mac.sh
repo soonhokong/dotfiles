87e7724fc3fb0b4a34583af1fc26d67d874be091
Soonho Kong <soonho.kong@tri.global>
2019-09-21 11:17:53 -0400

    Add scripts/update_mac.sh

---
+++ scripts/update_mac.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Update / upgrade packages installed via homebrew
+brew update && brew upgrade && brew cask upgrade && brew cleanup -s
+
+# LaTeX packages
+tlmgr update --all
+
+# PIP packages
+pipupgrade -y
+
+# Upgrade packages installed via Mac App Store
+mas upgrade
+
+# MacOS Software Update
+softwareupdate -i -a
+
+# Update / upgrade emacs packages
+emacs --no-window-system --eval "(auto-package-update-now)" --kill
